89cd4cf1f65313af863467d9335e81e70eac7c8d
Shehan-Amarasinghe <Shehan.nivantha124@gmail.com>
2024-06-10 12:43:55 +0530

    Update Project.jsx file

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -6,7 +6,7 @@ import To_do_list from '../assets/To-do-list.png'
 import Weather_App from '../assets/weather-app.png'
 import c_plus from '../assets/c_plus.png'
 import java from '../assets/Java.webp'
-
+import Projectitem from './Projectitem'
 
 
 
@@ -16,12 +16,12 @@ function Project() {
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
         <p className='py-8 text-center'>This is my latest projects</p>
 
-        <div className='grid gap-12 sm:grid-cols-4'>
+        <div className='grid gap-12 sm:grid-cols-2'>
             <Projectitem img={taking_note} title='Note taking app'/>
             <Projectitem img={outer_clove} title='Outer clove resturant'/>
             <Projectitem img={SLRailway_project} title='SL Railway ticket booking system'/>
             <Projectitem img={To_do_list} title='To-do-list app'/>
-            <Projectitem img={Weather_App} title='Weather app'/>
+            <Projectitem img={Weather_App} title='Weather-app'/>
             <Projectitem img={c_plus} title='Spire health care project C++'/>
             <Projectitem img={java} title='Apple istore project java'/>
         </div>
