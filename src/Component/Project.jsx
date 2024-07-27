eabf80c68292c0a6e0cb6ba2b806128aab492e25
Shehan-Amarasinghe <Shehan.nivantha124@gmail.com>
2024-07-27 17:19:22 +0530

    Add projects

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -4,9 +4,10 @@ import outer_clove from '../assets/outer-clove.png'
 import SLRailway_project from '../assets/sl-train.png'
 import To_do_list from '../assets/To-do-list.png'
 import Weather_App from '../assets/weather-app.png'
-import c_plus from '../assets/c_plus.png'
-import java from '../assets/Java.webp'
+import AxeL_Web from '../assets/Axel.png'
+import Dashboard from '../assets/Dashboard.png'
 import Projectitem from './Projectitem'
+
 
 
 
@@ -17,13 +18,13 @@ function Project() {
         <p className='py-8 text-center'>This is my latest projects</p>
 
         <div className='grid gap-12 sm:grid-cols-2'>
+            <Projectitem img={AxeL_Web} title='Axel Furniture Web'/>
+            <Projectitem img={Dashboard} title='Dashboard-Next.js'/>
             <Projectitem img={taking_note} title='Note taking app'/>
             <Projectitem img={outer_clove} title='Outer clove resturant'/>
             <Projectitem img={SLRailway_project} title='SL Railway ticket booking system'/>
             <Projectitem img={To_do_list} title='To-do-list app'/>
             <Projectitem img={Weather_App} title='Weather-app'/>
-            <Projectitem img={c_plus} title='Spire health care project C++'/>
-            <Projectitem img={java} title='Apple istore project java'/>
         </div>
     </div>
     
